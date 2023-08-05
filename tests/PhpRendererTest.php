3bed04e2c5c2cca34ee8a55071a837161707dd9a
devcoder-xyz <fadymichel@protonmail.com>
2023-08-05 20:30:56 +0000

    Add PhpRenderer class for rendering PHP templates with blocks and layout support

---
+++ tests/PhpRendererTest.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Test\DevCoder\Renderer;
+
+use DevCoder\Renderer\PhpRenderer;
+use PHPUnit\Framework\TestCase;
+
+final class PhpRendererTest extends TestCase
+{
+    private PhpRenderer $renderer;
+
+    protected function setUp(): void
+    {
+        $this->renderer = new PhpRenderer(__DIR__ . '/views');
+    }
+
+    public function testRenderBasicView(): void
+    {
+        $expectedOutput = 'Hello, World!';
+        $output = $this->renderer->render('test.php', ['message' => 'Hello, World!']);
+        $this->assertEquals($expectedOutput, $output);
+    }
+
+    public function testRenderViewWithLayout(): void
+    {
+        $expectedOutput = '<!DOCTYPE html><html lang="fr"><head><title></title></head><body class="body"></body></html>';
+        $output = $this->renderer->render('test_layout.php');
+        $this->assertEquals($expectedOutput, $output);
+    }
+
+    public function testRenderViewWithBlock(): void
+    {
+        $expectedOutput = '';
+        $output = $this->renderer->render('test_block.php');
+        $this->assertEquals($expectedOutput, $output);
+    }
+
+    public function testRenderViewWithExtendingBlock(): void
+    {
+        $expectedOutput = '<!DOCTYPE html><html lang="fr"><head><title>Page Title</title></head><body class="body">Page Content</body></html>';
+        $output = $this->renderer->render('test_extends.php');
+        $this->assertEquals($expectedOutput, $output);
+    }
+
+    public function testStartAndEndBlocks(): void
+    {
+        $this->renderer->startBlock('content');
+        echo 'Block Content';
+        $this->renderer->endBlock();
+
+        $expectedOutput = 'Block Content';
+        $output = $this->renderer->block('content');
+        $this->assertEquals($expectedOutput, $output);
+    }
+
+    public function testRenderViewNotFound(): void
+    {
+        $this->expectException(\InvalidArgumentException::class);
+        $this->expectExceptionMessage("non_existent_template.php template not found");
+        $this->renderer->render('non_existent_template.php');
+    }
+}
